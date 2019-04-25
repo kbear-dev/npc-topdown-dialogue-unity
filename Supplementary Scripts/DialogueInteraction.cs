f7a43c3be6d8e0f5b6dd0dbf7d5d8b6f5df8e689
DESKTOP-8QVSTJA\kbear <kyleedward.berran@benilde.edu.ph>
2019-04-26 01:27:06 +0800

    Added Supplementary Scripts for modular components
    Added Dialogue Interaction script for Player

---
+++ Supplementary Scripts/DialogueInteraction.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueInteraction : MonoBehaviour
+{
+    public KeyCode DialogueKey;
+
+    private NPC nearestNPC;
+    private bool isSignalActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nearestNPC = null;
+        isSignalActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!DialogueManager.isRunningDialogue)
+        {
+            if (isSignalActive)
+            {
+                if (Input.GetKeyDown(DialogueKey))
+                {
+                    nearestNPC.Talk();
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<NPC>() != null)
+        {
+            if (nearestNPC != null) return;
+
+            nearestNPC = collision.gameObject.GetComponent<NPC>();
+            nearestNPC.ShowSignal();
+            isSignalActive = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (nearestNPC != null)
+        {
+            nearestNPC.HideSignal();
+            nearestNPC = null;
+            isSignalActive = false;
+        }
+    }
+}
